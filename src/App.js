cc251298eb04b7920384749c5ae1b76967361be6
adityabisoi <adityabisoi1999@gmail.com>
2020-02-17 13:31:56 +0530

    tasks added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
-      <p for="vehicle1"> I have a bike</p>
-      <input type="checkbox" id="vehicle2" name="vehicle2" value="Car"></input>
-      <p for="vehicle2"> I have a car</p>
-      <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat"></input>
-      <p for="vehicle3"> I have a boat</p>
+      <input type="checkbox"></input>
+      <p> Task 1 </p>
+      <input type="checkbox"></input>
+      <p> Task 2 </p>
+      <input type="checkbox"></input>
+      <p> Task 3 </p>
     </div>
   );
 }
