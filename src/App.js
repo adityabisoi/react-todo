b1f521ae47a16fd730b6bcf81ed7e60353349bae
adityabisoi <adityabisoi1999@gmail.com>
2020-02-17 13:30:07 +0530

    App.js initialised with basic checkboxes and paragraphs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
+      <p for="vehicle1"> I have a bike</p>
+      <input type="checkbox" id="vehicle2" name="vehicle2" value="Car"></input>
+      <p for="vehicle2"> I have a car</p>
+      <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat"></input>
+      <p for="vehicle3"> I have a boat</p>
     </div>
   );
 }
