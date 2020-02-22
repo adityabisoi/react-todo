3f3addcf4d587431284dcc98fe3fd5541104ec4f
adityabisoi <adityabisoi1999@gmail.com>
2020-02-22 23:56:25 +0530

    stateful class

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import ToDoItem from './components/ToDoItem';
 import todosData from './todosData'
 import './App.css'
 
-function App() {
-  const todoData = todosData.map(todo => <ToDoItem key={todo.id} text={todo.text} completed={todo.completed} />)
-  return (
-    <div className="todo-list">
-        { todoData }
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+        todo : todosData
+    }
+  }
+  render(){
+    const todoData = this.state.todo.map(todo => <ToDoItem key={todo.id} text={todo.text} completed={todo.completed} />)
+    return(
+      <div className="todo-list">
+         { todoData }
+      </div>
+    );
+  }
 }
 
 export default App;
