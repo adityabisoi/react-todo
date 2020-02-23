d820ce7cf0088b24326e5a67738734a46fef41da
adityabisoi <adityabisoi1999@gmail.com>
2020-02-23 11:12:36 +0530

    onChange warning removed

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ToDoItem(props){
     return(
         <div className="todo-item">
-            <input type="checkbox" checked={props.completed}></input>
+            <input type="checkbox" checked={props.completed} onChange={() => console.log("Changed")}></input>
             <p style={{color: props.completed ? "gray":"black"}}> {props.text} </p>
         </div>        
     )
